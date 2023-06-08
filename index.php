14508e328eef37376df86c1c4471ab14124ee632
Hamdani17 <88165773+Hamdani17@users.noreply.github.com>
2023-06-08 13:05:02 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,22 +1,100 @@
 <?php
 
-require_once 'vendor/autoload.php';
+// Set up global variables
+$botToken = getenv('BOT_TOKEN');
+$apiBaseUrl = 'https://api.telegram.org/bot' . $botToken . '/';
+$types = [
+    'خماسي' => 'aa1a1',
+    'سداسي' => 'a11aa1',
+    'عشوائي' => 'aa1a111a1a'
+];
+$currentCheckingType = null;
 
-use Telethon\Telegram\Client;
-use Telethon\Session;
+// Helper function to generate a random username
+function generateUsername($pattern) {
+    $username = '';
+    for ($i = 0; $i < strlen($pattern); $i++) {
+        $char = $pattern[$i];
+        if ($char === 'a') {
+            $username .= chr(rand(97, 122)); // random lowercase letter
+        } elseif ($char === '1') {
+            $username .= rand(0, 9); // random digit
+        }
+    }
+    return $username;
+}
 
-$apiToken = '6022228026:AAH4MwFk3sNno832jZ9SorCsVbHBheK2c-0'; // Replace with your own API token
-$sessionFile = 'session'; // Path to your session file
+// Function to send a Telegram API request
+function sendTelegramRequest($method, $params = []) {
+    global $apiBaseUrl;
+    $url = $apiBaseUrl . $method;
 
-$session = new Session();
-$session->load($sessionFile);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, count($params));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
 
-$apiId = 'YOUR_API_ID'; // Replace with your own API ID
-$apiHash = 'YOUR_API_HASH'; // Replace with your own API hash
+    return json_decode($response, true);
+}
 
-$client = new Client($session, $apiId, $apiHash);
-$client->start();
+// Function to send a Telegram message
+function sendTelegramMessage($chatId, $text) {
+    $params = [
+        'chat_id' => $chatId,
+        'text' => $text
+    ];
+    sendTelegramRequest('sendMessage', $params);
+}
 
-// Your code for checking usernames goes here
+// Function to start checking usernames
+function startChecking($chatId, $type) {
+    global $currentCheckingType;
+    $currentCheckingType = $type;
 
-$client->run();
+    sendTelegramMessage($chatId, 'START CHECKING...!');
+    while ($currentCheckingType === $type) {
+        $username = generateUsername($type);
+        // Here, you can add the logic to check if the generated username is available on Telegram
+        // If the username is available, send it using the sendTelegramMessage function
+        // For example:
+        // if (isUsernameAvailable($username)) {
+        //     sendTelegramMessage($chatId, $username);
+        // }
+        // You can replace the isUsernameAvailable function with your own logic to check username availability
+
+        // Wait for a while before checking the next username
+        sleep(1);
+    }
+}
+
+// Handle incoming updates
+$update = json_decode(file_get_contents('php://input'), true);
+
+if (isset($update['message'])) {
+    $message = $update['message'];
+    $chatId = $message['chat']['id'];
+    $text = $message['text'];
+
+    if ($text === '/start') {
+        $typesText = "Types:\n";
+        foreach ($types as $typeName => $typePattern) {
+            $typesText .= $typeName . ': ' . $typePattern . "\n";
+        }
+        sendTelegramMessage($chatId, $typesText);
+    } elseif (strpos($text, '/check') === 0) {
+        $type = trim(substr($text, 7));
+        if (array_key_exists($type, $types)) {
+            startChecking($chatId, $type);
+        } else {
+            sendTelegramMessage($chatId, 'Please choose a valid type.');
+        }
+    } elseif ($text === '/stop') {
+        $currentCheckingType = null;
+        sendTelegramMessage($chatId, 'Username checking stopped.');
+    }
+}
+
+?>
